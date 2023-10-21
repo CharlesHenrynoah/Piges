60b425f2e0593bf04b08d4c4581e65ea734b0811
Noah <noahcharleshenry4@gmail.com>
2023-10-21 13:24:55 +0200

    création du module TestUi

--- piges/src/App.js
+++ piges/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Test from "./components/templates/testbdd";
 import Login from "./components/templates/login/login";
 import Info from "./components/templates/info/info";
+import TestUI from './components/templates/testui';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/test" element={<Test />} />
           <Route path="/login" element={<Login />} />
           <Route path="/info/:prenom" element={<Info />} /> {/* Nouvelle route pour le composant Info */}
+          <Route path="/testui" element={<TestUI />} />
         </Routes>
       </div>
     </Router>
---
+++ piges/src/components/templates/testui.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestUI = () => {
+  return (
+    <div>
+      <h1>Modèle de test</h1>
+      <p>Ceci est un modèle de test simple.</p>
+    </div>
+  );
+};
+
+export default TestUI;
